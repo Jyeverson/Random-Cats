af3033cda3cbd9702b3dd6bfdd1b8a2b046fd658
jyeverson <jyeversonsantos@gmail.com>
2023-04-19 18:55:29 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const catFactEl = document.getElementById('cat-fact');
+const catImageEl = document.getElementById('cat-image');
+const newCatButton = document.getElementById('new-cat-button');
+
+function getCatFact() {
+  fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1')
+    .then(response => response.json())
+    .then(data => {
+      catFactEl.textContent = data[0].text;
+    })
+    .catch(error => {
+      //catFactEl.textContent = 'Oops! Algo deu errado.';
+      console.error(error);
+    });
+}
+
+function getCatImage() {
+  fetch('https://api.thecatapi.com/v1/images/search?limit=1&size=med')
+    .then(response => response.json())
+    .then(data => {
+      catImageEl.src = data[0].url;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+newCatButton.addEventListener('click', () => {
+  getCatFact();
+  getCatImage();
+});
+
+getCatFact();
+getCatImage();
